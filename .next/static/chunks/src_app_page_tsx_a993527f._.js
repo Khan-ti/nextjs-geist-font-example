9ac38d2791dab90443d335ea14249bc506e8eff9
Khan-ti <75572484+Khan-ti@users.noreply.github.com>
2025-07-08 17:01:21 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751968881396

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b2f05e6b._.js",
+  "static/chunks/src_6325f20f._.js"
+],
+    source: "dynamic"
+});
